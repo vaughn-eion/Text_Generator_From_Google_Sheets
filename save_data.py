7bc12daf48acac65e5564ab010fb208cf03b1cc1
Eion Vaughn <108884474+vaughn-eion@users.noreply.github.com>
2023-02-23 21:17:43 -0500

    Update save_data.py

--- save_data.py
+++ save_data.py
@@ -1,9 +1,10 @@
 import google_connector as gc
 import pandas as pd
-from datetime import date
 
+# Columns from google sheets
 cols = ['Username', 'Email', 'Address', 'Title', 'Salary']
 
+# Save data from google sheets into local csv so there is not a need to constantly call the api
 def save_data(data):
     df = pd.DataFrame(data, columns=cols)
     df = df.tail(-1)
